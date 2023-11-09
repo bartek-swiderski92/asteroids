daa5e2ab3f04bff48892d87af6f7ffd3ddbad4e4
Bartek Swiderski <phos23@gmail.com>
2023-11-09 18:09:29 +0000

    created transforming loop

--- exercise-tasks/ex4-svg/drawing.js
+++ exercise-tasks/ex4-svg/drawing.js
@@ -4,10 +4,26 @@ import svg_ from '../modules/svg-game.js';
 const asteroids = document.querySelector('#asteroids');
 
 svg_.drawGrid(asteroids);
-svg_.drawShip(asteroids, 125, {guide: true});
+// svg_.drawShip(asteroids, 125, {guide: true});
 
-let t = asteroids.clientWidth;
-let r = Math.PI / 500;
+// let t = asteroids.clientWidth;
+// let r = Math.PI / 500;
 
-let path = document.querySelector('path');
-path.style.display = 'none';
+// let path = document.querySelector('path');
+// path.style.display = 'none';
+svg_.drawShip(asteroids, 20, 20, 15, {guide: true, rotate: 0.8});
+let rotateValue = 0;
+for (let y = 0; y < 400; y += 40) {
+    for (let x = 0; x < 400; x += 40) {
+        if (y === 0 && x === 0) continue;
+        rotateValue += 1.5;
+        let ogShip = document.querySelector('path');
+        let ogCircle = document.querySelector('circle');
+        let clonedShip = ogShip.cloneNode(true);
+        let clonedCircle = ogCircle.cloneNode(true);
+        clonedShip.setAttribute('transform', `translate(${x}, ${y}) rotate(${rotateValue}, 20, 20)`);
+        clonedCircle.setAttribute('transform', `translate(${x}, ${y})`);
+        asteroids.appendChild(clonedShip);
+        asteroids.appendChild(clonedCircle);
+    }
+}
