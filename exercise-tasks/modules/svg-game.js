06a5ac949ffad11d770cab6323c5f34dce7a6801
Bartek Swiderski <phos23@gmail.com>
2024-02-02 15:34:26 +0000

    reorder methods

--- exercise-tasks/modules/svg-game.js
+++ exercise-tasks/modules/svg-game.js
@@ -3,6 +3,21 @@
 const svg_ = {};
 const svg_private = {};
 
+svg_.rotateElement = function (asteroids, element, position, x, y, rotateValue) {
+    let clonedElement = element.cloneNode(true);
+    clonedElement.setAttribute('transform', `translate(${x}, ${y}) rotate(${rotateValue}, ${position}, ${position})`);
+    asteroids.appendChild(clonedElement);
+};
+
+svg_private.setBasicAttributes = function (elementType, className, fill, stroke, strokeWidth) {
+    let element = document.createElementNS('http://www.w3.org/2000/svg', elementType);
+    element.setAttribute('class', className);
+    element.setAttribute('fill', fill);
+    element.setAttribute('stroke', stroke);
+    element.setAttribute('stroke-width', strokeWidth);
+    return element;
+};
+
 svg_.drawGrid = function (asteroids, minor = 10, major = minor * 5, lineColor = '#00FF00', textColor = '#009900') {
     const boardWidth = asteroids.clientWidth;
     const boardHeight = asteroids.clientHeight;
@@ -65,6 +80,42 @@ svg_.drawPath = function (asteroids, coordinates, strokeWidth, stroke, fill, clo
     }
 };
 
+svg_.drawCircle = function (x, y, radius, fill = 'rgba(0, 0, 0, 0.405)', stroke = 'white', strokeWidth = '0.5px') {
+    let circleEl = svg_private.setBasicAttributes('circle', 'guide-circle', fill, stroke, strokeWidth);
+    circleEl.setAttribute('cx', x);
+    circleEl.setAttribute('cy', y);
+    circleEl.setAttribute('r', radius);
+
+    return circleEl;
+};
+
+svg_.drawShip = function (asteroids, position, radius, options = {}) {
+    //defaults:
+    let lineWidth = options.lineWidth || 0.5;
+    let stroke = options.stroke || 'white';
+    let fill = options.fill || 'black';
+    let angle = (options.angle || 0.5 * Math.PI) / 2;
+    let curve1 = options.curve1 || 0.25;
+    let curve2 = options.curve2 || 0.75;
+    let guide = options.guide;
+
+    if (guide) {
+        let guideCircle = svg_.drawCircle(position, position, radius);
+        asteroids.appendChild(guideCircle);
+    }
+    svg_private.drawShipPaths(asteroids, position, radius, angle, curve1, curve2, guide, lineWidth, stroke, fill);
+};
+
+svg_private.drawShipPaths = function (asteroids, position, radius, angle, curve1, curve2, guide, strokeWidth, stroke, fill) {
+    const coordinates = svg_private.buildShipCoordinatesObject(position, radius, angle, curve1, curve2);
+    const dAttribute = svg_private.buildDAttributeForShip(position, coordinates, guide, radius);
+
+    const outputElement = svg_private.setBasicAttributes('path', 'ship', fill, stroke, strokeWidth);
+
+    outputElement.setAttribute('d', dAttribute);
+    asteroids.appendChild(outputElement);
+};
+
 svg_private.buildShipCoordinatesObject = function (position, radius, angle, curve1, curve2) {
     return {
         startingPoint: {posX: position + radius, posY: position},
@@ -97,18 +148,6 @@ svg_private.buildShipCoordinatesObject = function (position, radius, angle, curv
     };
 };
 
-svg_private.buildAsteroidCoordinatesString = function (x, y, radius, segments, noise) {
-    let coordinates = 'M';
-    for (let i = 0; i < segments; i++) {
-        const angle = (i / segments) * 2 * Math.PI;
-        const x1 = radius * Math.cos(angle) * ((Math.random() + 0.6) * noise) + radius;
-        const y1 = radius * Math.sin(angle) * ((Math.random() + 0.6) * noise) + radius;
-        coordinates += `${x1}, ${y1} `;
-    }
-    coordinates += 'Z';
-    return coordinates;
-};
-
 svg_private.buildDAttributeForShip = function (position, coordinates, guide, radius) {
     let dAttribute = `M ${coordinates.startingPoint.posX} ${coordinates.startingPoint.posY}`;
     coordinates.waypoints.forEach((waypoint) => {
@@ -127,70 +166,6 @@ svg_private.buildDAttributeForShip = function (position, coordinates, guide, rad
     return dAttribute;
 };
 
-svg_private.setBasicAttributes = function (elementType, className, fill, stroke, strokeWidth) {
-    let element = document.createElementNS('http://www.w3.org/2000/svg', elementType);
-    element.setAttribute('class', className);
-    element.setAttribute('fill', fill);
-    element.setAttribute('stroke', stroke);
-    element.setAttribute('stroke-width', strokeWidth);
-    return element;
-};
-
-svg_private.drawShipPaths = function (asteroids, position, radius, angle, curve1, curve2, guide, strokeWidth, stroke, fill) {
-    const coordinates = svg_private.buildShipCoordinatesObject(position, radius, angle, curve1, curve2);
-    const dAttribute = svg_private.buildDAttributeForShip(position, coordinates, guide, radius);
-
-    const outputElement = svg_private.setBasicAttributes('path', 'ship', fill, stroke, strokeWidth);
-
-    outputElement.setAttribute('d', dAttribute);
-    console.log(outputElement);
-    asteroids.appendChild(outputElement);
-};
-svg_private.crateAsteroidsElement = function (asteroids, x, y, radius, segments, lineWidth, stroke, fill, noise) {
-    const dAttribute = svg_private.buildAsteroidCoordinatesString(x, y, radius, segments, noise);
-    const pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-
-    pathElement.setAttribute('d', dAttribute);
-    pathElement.setAttribute('stroke', stroke);
-    pathElement.setAttribute('fill', fill);
-    pathElement.setAttribute('stroke-width', lineWidth);
-
-    pathElement.style.transform = `translate(${x - radius}px, ${y - radius}px)`;
-    return pathElement;
-};
-
-svg_.drawCircle = function (x, y, radius, fill = 'rgba(0, 0, 0, 0.405)', stroke = 'white', strokeWidth = '0.5px') {
-    let circleEl = svg_private.setBasicAttributes('circle', 'guide-circle', fill, stroke, strokeWidth);
-    circleEl.setAttribute('cx', x);
-    circleEl.setAttribute('cy', y);
-    circleEl.setAttribute('r', radius);
-
-    return circleEl;
-};
-
-svg_.drawShip = function (asteroids, position, radius, options = {}) {
-    //defaults:
-    let lineWidth = options.lineWidth || 0.5;
-    let stroke = options.stroke || 'white';
-    let fill = options.fill || 'black';
-    let angle = (options.angle || 0.5 * Math.PI) / 2;
-    let curve1 = options.curve1 || 0.25;
-    let curve2 = options.curve2 || 0.75;
-    let guide = options.guide;
-
-    if (guide) {
-        let guideCircle = svg_.drawCircle(position, position, radius);
-        asteroids.appendChild(guideCircle);
-    }
-    svg_private.drawShipPaths(asteroids, position, radius, angle, curve1, curve2, guide, lineWidth, stroke, fill);
-};
-
-svg_.rotateElement = function (asteroids, element, position, x, y, rotateValue) {
-    let clonedElement = element.cloneNode(true);
-    clonedElement.setAttribute('transform', `translate(${x}, ${y}) rotate(${rotateValue}, ${position}, ${position})`);
-    asteroids.appendChild(clonedElement);
-};
-
 svg_.drawAsteroid = function (asteroids, x, y, radius, segments, options = {}) {
     //defaults:
     let lineWidth = options.lineWidth ?? 1.75;
@@ -207,6 +182,31 @@ svg_.drawAsteroid = function (asteroids, x, y, radius, segments, options = {}) {
     const asteroidsElement = svg_private.crateAsteroidsElement(asteroids, x, y, radius, segments, lineWidth, stroke, fill, noise);
 
     asteroids.appendChild(asteroidsElement);
+};
+
+svg_private.crateAsteroidsElement = function (asteroids, x, y, radius, segments, lineWidth, stroke, fill, noise) {
+    const dAttribute = svg_private.buildAsteroidCoordinatesString(x, y, radius, segments, noise);
+    const pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+    pathElement.setAttribute('d', dAttribute);
+    pathElement.setAttribute('stroke', stroke);
+    pathElement.setAttribute('fill', fill);
+    pathElement.setAttribute('stroke-width', lineWidth);
+
+    pathElement.style.transform = `translate(${x - radius}px, ${y - radius}px)`;
+    return pathElement;
+};
+
+svg_private.buildAsteroidCoordinatesString = function (x, y, radius, segments, noise) {
+    let coordinates = 'M';
+    for (let i = 0; i < segments; i++) {
+        const angle = (i / segments) * 2 * Math.PI;
+        const x1 = radius * Math.cos(angle) * ((Math.random() + 0.6) * noise) + radius;
+        const y1 = radius * Math.sin(angle) * ((Math.random() + 0.6) * noise) + radius;
+        coordinates += `${x1}, ${y1} `;
+    }
+    coordinates += 'Z';
+    return coordinates;
 };
 
 export default svg_;
